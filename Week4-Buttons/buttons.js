810ac0c63e85b8d9e40ad7ca1cf5b4bdde352383
David G <115465436+daveuat@users.noreply.github.com>
2022-11-13 13:42:12 +0900

    Delete Week4-Buttons directory

--- Week4-Buttons/buttons.js
+++
@@ -1,8 +0,0 @@
-function start(){ //function start. when clicked it will grey out the button afterwards
-    document.getElementById("startButton").disabled = true;
-    document.getElementById("stopButton").disabled = false;
-}
-function stop(){ //function stop. when stop is clicked it will grey it out and it wont reactive till user presses start
-    document.getElementById("stopButton").disabled = true;
-    document.getElementById("startButton").disabled = false;
-}
